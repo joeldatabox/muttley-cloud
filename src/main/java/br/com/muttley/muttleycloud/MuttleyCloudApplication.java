813304284e119337f0f22c29f141f5a2606f3e7f
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-01-24 17:55:05 -0200

    commit initial

---
+++ src/main/java/br/com/muttley/muttleycloud/MuttleyCloudApplication.java
@@ -0,0 +1,12 @@
+package br.com.muttley.muttleycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuttleyCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuttleyCloudApplication.class, args);
+	}
+}
