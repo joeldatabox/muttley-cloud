c2dc13e8dc4401136e563eb9138ed5a95072bf3b
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-08-13 12:30:23 -0300

    ajustes de segurança

--- muttley-config-server/src/main/java/br/com/muttley/configserver/autoconfig/SecurityConfig.java
+++ muttley-config-server/src/main/java/br/com/muttley/configserver/autoconfig/SecurityConfig.java
@@ -3,11 +3,15 @@ package br.com.muttley.configserver.autoconfig;
 import br.com.muttley.configserver.property.MuttleyConfigServerProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Configuração padrão de segurança para o serviço de config
@@ -29,12 +33,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * Configurando usuário e senha necessário para autenticação no serviço
      */
-    @Autowired
-    public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    public void configure(final AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
+                .passwordEncoder(createPasswordEncoder())
                 .withUser(this.property.getSecurity().getUser().getName())
-                .password(this.property.getSecurity().getUser().getPassword())
-                .roles(this.property.getSecurity().getUser().getRole());
+                .password(
+                        createPasswordEncoder().encode(this.property.getSecurity().getUser().getPassword())
+                ).roles(this.property.getSecurity().getUser().getRole());
+    }
+
+    @Bean
+    public PasswordEncoder createPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     /**
@@ -47,6 +58,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .hasRole(this.property.getSecurity().getUser().getRole())
                 .and()
                 .httpBasic()
+                .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf()
                 .disable();
