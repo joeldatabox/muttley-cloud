f4270c3685bb5ffbfeba29d059830033df3ceb09
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-08-30 14:58:33 -0300

    adicionado submodulo de segurança

---
+++ muttley-security.pom/muttley-security-gateway/src/main/java/br/com/invistate/muttleysercuritygateway/MuttleySercurityGatewayApplication.java
@@ -0,0 +1,12 @@
+package br.com.invistate.muttleysercuritygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuttleySercurityGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuttleySercurityGatewayApplication.class, args);
+	}
+}
---
+++ muttley-security.pom/muttley-security-service/src/main/java/br/com/invistate/muttleysercurityservice/MuttleySercurityServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.invistate.muttleysercurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuttleySercurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuttleySercurityServiceApplication.class, args);
+	}
+}
