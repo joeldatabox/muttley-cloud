be47a865b2996fafcf75ee566f58cb21968b049a
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-08-31 20:16:30 -0300

    ajustes de beans

---
+++ muttley-security.pom/muttley-security-gateway/src/main/java/br/com/muttley/security/gateway/MuttleySecurityAutoconfig.java
@@ -0,0 +1,15 @@
+package br.com.muttley.security.gateway;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Joel Rodrigues Moreira on 31/08/18.
+ * e-mail: <a href="mailto:joel.databox@gmail.com">joel.databox@gmail.com</a>
+ * @project muttley-cloud
+ * <p>
+ */
+@Configuration
+@ComponentScan(basePackages = "br.com.muttley.security.gateway.config")
+public class MuttleySecurityAutoconfig {
+}
