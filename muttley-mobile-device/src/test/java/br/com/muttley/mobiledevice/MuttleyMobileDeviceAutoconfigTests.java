8834784cdf1a9862fef5711468c7c7defe2332b4
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-09-05 10:13:26 -0300

    adicionado starter para spring.mobile.device

---
+++ muttley-mobile-device/src/test/java/br/com/muttley/mobiledevice/MuttleyMobileDeviceAutoconfigTests.java
@@ -0,0 +1,14 @@
+package br.com.muttley.mobiledevice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class MuttleyMobileDeviceAutoconfigTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
