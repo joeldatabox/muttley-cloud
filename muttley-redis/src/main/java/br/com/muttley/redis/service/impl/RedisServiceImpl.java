752d8309adf41a1ac3ae02db1344d03b3cf7915d
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-11-03 11:15:05 -0300

    Adicionado a opção de TTL

--- muttley-redis/src/main/java/br/com/muttley/redis/service/impl/RedisServiceImpl.java
+++ muttley-redis/src/main/java/br/com/muttley/redis/service/impl/RedisServiceImpl.java
@@ -13,12 +13,12 @@ import org.springframework.data.redis.serializer.SerializationException;
 
 import javax.annotation.PostConstruct;
 import java.util.Collection;
-import java.util.concurrent.TimeUnit;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY;
 import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
 import static com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping.NON_FINAL;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 /**
  * @author Joel Rodrigues Moreira on 08/01/18.
@@ -56,8 +56,14 @@ public class RedisServiceImpl<T> implements RedisService<T> {
     @Override
     public RedisService set(final String key, final T value, final long time) {
         final String keyValue = createKey(key);
-        this.redisTemplate.opsForValue().set(createKey(key), value, time, TimeUnit.MILLISECONDS);
+        this.redisTemplate.opsForValue().set(createKey(key), value, time, MILLISECONDS);
         return this;
+    }
+
+    @Override
+    public Long getExpire(String key) {
+        this.redisTemplate.getExpire(createKey(key), MILLISECONDS);
+        return null;
     }
 
     @Override
