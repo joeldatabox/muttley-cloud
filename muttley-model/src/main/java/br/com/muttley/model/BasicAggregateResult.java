e5e6afa0fd1332fb443a713476e5b64aca5ac678
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-06-21 10:14:51 -0300

    Classe basica para agregações

---
+++ muttley-model/src/main/java/br/com/muttley/model/BasicAggregateResult.java
@@ -0,0 +1,22 @@
+package br.com.muttley.model;
+
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author Joel Rodrigues Moreira on 19/06/19.
+ * e-mail: <a href="mailto:joel.databox@gmail.com">joel.databox@gmail.com</a>
+ * @project muttley-cloud
+ */
+@EqualsAndHashCode(of = "result")
+public class BasicAggregateResult<T> {
+    private T result;
+
+    public T getResult() {
+        return result;
+    }
+
+    public BasicAggregateResult<T> setResult(final T result) {
+        this.result = result;
+        return this;
+    }
+}
