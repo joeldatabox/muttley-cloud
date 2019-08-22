4c7e5cc8d595a5063cd462b984b6cd48bdb2a158
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-08-22 10:41:58 -0300

    adicionado anotações do mongo

--- muttley-mongo/src/main/java/br/com/muttley/mongo/converters/Decimal128ToBigDecimalConverter.java
+++ muttley-mongo/src/main/java/br/com/muttley/mongo/converters/Decimal128ToBigDecimalConverter.java
@@ -6,11 +6,13 @@ package br.com.muttley.mongo.converters;
 
 import org.bson.types.Decimal128;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
+@ReadingConverter
 public class Decimal128ToBigDecimalConverter implements Converter<Decimal128, BigDecimal> {
 
     @Override
