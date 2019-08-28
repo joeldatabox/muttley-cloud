bbb7bf01cccdf4724c86c7dcf15f5884f3dc18e0
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-08-28 13:15:55 -0300

    criado metodo que busca varios ids em uma unica requisição

--- muttley-domain-service/src/main/java/br/com/muttley/domain/service/impl/ModelServiceImpl.java
+++ muttley-domain-service/src/main/java/br/com/muttley/domain/service/impl/ModelServiceImpl.java
@@ -94,6 +94,11 @@ public abstract class ModelServiceImpl<T extends Model> extends ServiceImpl<T> i
         if (ObjectUtils.isEmpty(ids)) {
             throw new MuttleyBadRequestException(clazz, "id", "informe pelo menos um id válido");
         }
+        if (ids.length > 50) {
+            throw new MuttleyBadRequestException(clazz, "ids", "Quantidade máxima excedida")
+                    .addDetails("min", 1)
+                    .addDetails("max", 50);
+        }
         final Set<T> records = this.repository.findMulti(user.getCurrentOwner(), ids);
         if (records == null) {
             return Collections.emptySet();
