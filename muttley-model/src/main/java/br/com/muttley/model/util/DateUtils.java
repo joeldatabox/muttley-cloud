100486402031bcf315c7585bfc13b5f3a6931091
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-11-08 11:42:31 -0300

    Adicionado metodo extras

--- muttley-model/src/main/java/br/com/muttley/model/util/DateUtils.java
+++ muttley-model/src/main/java/br/com/muttley/model/util/DateUtils.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
+import static java.time.temporal.TemporalAdjusters.firstDayOfMonth;
+import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
+
 /**
  * @author Joel Rodrigues Moreira on 18/06/18.
  * e-mail: <a href="mailto:joel.databox@gmail.com">joel.databox@gmail.com</a>
@@ -16,5 +19,83 @@ public class DateUtils {
 
     public static LocalDateTime toLocalDateTime(final Date date, final ZoneId zoneId) {
         return LocalDateTime.ofInstant(date.toInstant(), zoneId);
+    }
+
+    public static boolean isEquals(final Date dateIni, final Date dateEnd) {
+        return toLocalDateTime(dateIni).isEqual(toLocalDateTime(dateEnd));
+    }
+
+    public static boolean isAfter(final Date dateIni, final Date dateEnd) {
+        return toLocalDateTime(dateIni).isAfter(toLocalDateTime(dateEnd));
+    }
+
+    public static boolean isBefore(final Date dateIni, final Date dateEnd) {
+        return toLocalDateTime(dateIni).isBefore(toLocalDateTime(dateEnd));
+    }
+
+    public static boolean lessThanOrEqualTo(final Date dateIni, final Date dateEnd) {
+        final LocalDateTime localDateIni = toLocalDateTime(dateIni);
+        final LocalDateTime localDateEnd = toLocalDateTime(dateEnd);
+        return localDateIni.isEqual(localDateEnd) || localDateIni.isBefore(localDateEnd);
+    }
+
+    public static boolean lessThanOrEqualTo(final LocalDateTime dateIni, final LocalDateTime dateEnd) {
+        return dateIni.isEqual(dateEnd) || dateIni.isBefore(dateEnd);
+    }
+
+    public static boolean greaterthanOrEqualTo(final Date dateIni, final Date dateEnd) {
+        final LocalDateTime localDateIni = toLocalDateTime(dateIni);
+        final LocalDateTime localDateEnd = toLocalDateTime(dateEnd);
+        return localDateIni.isEqual(localDateEnd) || localDateIni.isAfter(localDateEnd);
+    }
+
+    public static boolean greaterthanOrEqualTo(final LocalDateTime localDateIni, final LocalDateTime localDateEnd) {
+        return localDateIni.isEqual(localDateEnd) || localDateIni.isAfter(localDateEnd);
+    }
+
+    public static Date toFirstHour(final Date date) {
+        return toDate(toFirstHour(toLocalDateTime(date)));
+    }
+
+    public static LocalDateTime toFirstHour(final LocalDateTime date) {
+        return date
+                .withHour(0)
+                .withMinute(0)
+                .withSecond(0)
+                .withNano(0);
+    }
+
+    public static Date toLastHour(final Date date) {
+        return toDate(toLastHour(toLocalDateTime(date)));
+    }
+
+    public static LocalDateTime toLastHour(final LocalDateTime date) {
+        return date
+                .withHour(23)
+                .withMinute(59)
+                .withSecond(59)
+                .withNano(999999999);
+    }
+
+    public static Date toFirstDayOfMonth(final Date date) {
+        return toDate(toFirstDayOfMonth(toLocalDateTime(date)));
+    }
+
+    public static LocalDateTime toFirstDayOfMonth(final LocalDateTime date) {
+        return date.with(firstDayOfMonth());
+    }
+
+    public static Date toLastDayOfMonth(final Date date) {
+        return toDate(toLastDayOfMonth(toLocalDateTime(date)));
+    }
+
+    public static LocalDateTime toLastDayOfMonth(final LocalDateTime date) {
+        return date.with(lastDayOfMonth());
+    }
+
+    public static Date toDate(final LocalDateTime date) {
+        return Date.from(
+                date.atZone(ZoneId.systemDefault()).toInstant()
+        );
     }
 }
