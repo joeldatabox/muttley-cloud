c720e0b878de955f8ab56befbb02de6786bc023f
Joel Rodrigues Moreira <joel.databox@gmail.com>
2020-05-13 22:20:25 -0300

    Adicionado o modulo de header

--- muttley-headers/src/main/java/br/com/muttley/headers/ConfigRequestMetadata.java
+++ muttley-headers/src/main/java/br/com/muttley/headers/ConfigRequestMetadata.java
@@ -1,7 +1,11 @@
 package br.com.muttley.headers;
 
+import br.com.muttley.headers.components.MuttleyCurrentTimezone;
+import br.com.muttley.headers.components.MuttleyCurrentVersion;
 import br.com.muttley.headers.components.MuttleyUserAgent;
+import br.com.muttley.headers.components.MuttleyUserAgentName;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -24,22 +28,22 @@ public class ConfigRequestMetadata {
         return new MuttleyUserAgent(request);
     }
 
-    /*@Bean(name = "currentTimezone")
+    @Bean(name = "currentTimezone")
     @Scope(value = SCOPE_REQUEST, proxyMode = TARGET_CLASS)
     public MuttleyCurrentTimezone getCurrentTimezone(@Autowired final HttpServletRequest request) {
         return new MuttleyCurrentTimezone(request);
-    }*/
+    }
 
-    /*@Bean(name = "currentVersion")
+    @Bean(name = "currentVersion")
     @Scope(value = SCOPE_REQUEST, proxyMode = TARGET_CLASS)
     @Autowired
     public MuttleyCurrentVersion getCurrentVersion(final HttpServletRequest request, final BuildProperties buildProperties) {
         return new MuttleyCurrentVersion(request, buildProperties);
-    }*/
+    }
 
-    /*@Bean(name = "userAgentName")
+    @Bean(name = "userAgentName")
     @Scope(value = SCOPE_REQUEST, proxyMode = TARGET_CLASS)
     public MuttleyUserAgentName getUserAgentName(@Autowired final HttpServletRequest request) {
         return new MuttleyUserAgentName(request);
-    }*/
+    }
 }
