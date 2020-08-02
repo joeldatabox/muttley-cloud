f200280bb7bceaeb257cc6c15299f01673ea50e0
Joel Rodrigues Moreira <joel.databox@gmail.com>
2020-08-02 18:09:54 -0300

    setando scale no bigdecimal ao recuperar dados

--- muttley-mongo/src/main/java/br/com/muttley/mongo/converters/Decimal128ToBigDecimalConverter.java
+++ muttley-mongo/src/main/java/br/com/muttley/mongo/converters/Decimal128ToBigDecimalConverter.java
@@ -1,9 +1,5 @@
 package br.com.muttley.mongo.converters;
 
-/**
- * Created by master on 16/06/17.
- */
-
 import org.bson.types.Decimal128;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
@@ -11,12 +7,14 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+import static br.com.muttley.model.util.BigDecimalUtil.setDefaultScale;
+
 @Component
 @ReadingConverter
 public class Decimal128ToBigDecimalConverter implements Converter<Decimal128, BigDecimal> {
 
     @Override
     public BigDecimal convert(final Decimal128 source) {
-        return source == null ? null : source.bigDecimalValue();
+        return source == null ? null : setDefaultScale(source.bigDecimalValue());
     }
 }
