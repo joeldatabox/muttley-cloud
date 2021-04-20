93b784586ad03d7d52927635c059a488fa93f1d4
Joel Rodrigues Moreira <joel.databox@gmail.com>
2021-04-20 16:38:36 -0300

    adicionado o servidor de admin

---
+++ muttley-admin-server.pom/muttley-admin-server/src/test/java/br/com/muttley/muttleyadminserver/MuttleyAdminServerApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.muttley.muttleyadminserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MuttleyAdminServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
