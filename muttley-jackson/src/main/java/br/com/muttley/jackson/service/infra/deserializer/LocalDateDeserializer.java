7cfc2d3698d4a12bc56d4184058b7bdff292a83e
Joel Rodrigues Moreira <joel.databox@gmail.com>
2021-06-30 12:28:32 -0300

    ajustes de deserializador para datas

--- muttley-jackson/src/main/java/br/com/muttley/jackson/service/infra/deserializer/LocalDateDeserializer.java
+++ muttley-jackson/src/main/java/br/com/muttley/jackson/service/infra/deserializer/LocalDateDeserializer.java
@@ -10,10 +10,14 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 import static br.com.muttley.model.util.DateUtils.DEFAULT_ISO_LOCAL_DATE;
 import static br.com.muttley.model.util.DateUtils.DEFAULT_ISO_ZONED_DATE_TIME;
+import static java.util.Arrays.asList;
 
 /**
  * @author Joel Rodrigues Moreira on 17/06/2021.
@@ -21,7 +25,7 @@ import static br.com.muttley.model.util.DateUtils.DEFAULT_ISO_ZONED_DATE_TIME;
  * @project muttley-cloud
  */
 public class LocalDateDeserializer extends JsonDeserializer<LocalDate> {
-    private static final String END_WITH = "T00:00:00.000-";
+    private static final Pattern DATE_PATTERN = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
 
     @Override
     public LocalDate deserialize(final JsonParser parser, final DeserializationContext context) throws IOException, JsonProcessingException {
@@ -32,14 +36,19 @@ public class LocalDateDeserializer extends JsonDeserializer<LocalDate> {
         }
         final String value = node.asText();
         try {
-            if (value.contains(END_WITH)) {
-                return LocalDate.parse(value, DEFAULT_ISO_ZONED_DATE_TIME);
+            if (this.isValideDate(value)) {
+                return LocalDate.parse(value, DEFAULT_ISO_LOCAL_DATE);
             }
-            return LocalDate.parse(value, DEFAULT_ISO_LOCAL_DATE);
+            return LocalDate.parse(value, DEFAULT_ISO_ZONED_DATE_TIME);
+
         } catch (DateTimeParseException e) {
             throw new MuttleyBadRequestException(null, parser.getCurrentName(), "Informe uma data válida ")
                     .addDetails("informado", value)
-                    .addDetails("exemplo", LocalDate.now().format(DEFAULT_ISO_LOCAL_DATE));
+                    .addDetails("exemplos", asList(LocalDateTime.now().format(DEFAULT_ISO_ZONED_DATE_TIME), LocalDate.now().format(DEFAULT_ISO_LOCAL_DATE)));
         }
+    }
+
+    private boolean isValideDate(final String value) {
+        return DATE_PATTERN.matcher(value).matches();
     }
 }
