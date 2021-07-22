2713c45daac865583de198384be1e64686485b14
Joel Rodrigues Moreira <joel.databox@gmail.com>
2021-07-21 23:13:37 -0300

    metodo para comparação de colections

---
+++ muttley-model/src/main/java/br/com/muttley/model/util/CollectionUtils.java
@@ -0,0 +1,30 @@
+package br.com.muttley.model.util;
+
+import java.util.Collection;
+import java.util.Objects;
+/**
+ * @author Joel Rodrigues Moreira on 21/07/2021.
+ * e-mail: <a href="mailto:joel.databox@gmail.com">joel.databox@gmail.com</a>
+ * @project muttley-cloud
+ */
+public class CollectionUtils {
+    public static boolean isEquals(final Collection first, final Collection second) {
+        if ((first == null || first.isEmpty()) && (second == null || second.isEmpty())) {
+            return true;
+        }
+        if ((first == null && second != null) || (first != null && second == null)) {
+            return false;
+        }
+        if (first.size() != second.size()) {
+            return false;
+        }
+
+        return first
+                .parallelStream()
+                .filter(f ->
+                        second.parallelStream()
+                                .filter(s -> Objects.equals(s, f))
+                                .count() < 1
+                ).count() == 0;
+    }
+}
