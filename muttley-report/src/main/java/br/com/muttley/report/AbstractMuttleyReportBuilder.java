1dcb879affb722db789aa5a3d4db2651c90f9dcd
Joel Rodrigues Moreira <joel.databox@gmail.com>
2022-01-07 14:17:38 -0300

    ajustes para geração de relatório

--- muttley-report/src/main/java/br/com/muttley/report/AbstractMuttleyReportBuilder.java
+++ muttley-report/src/main/java/br/com/muttley/report/AbstractMuttleyReportBuilder.java
@@ -3,6 +3,7 @@ package br.com.muttley.report;
 import br.com.muttley.headers.components.MuttleyCurrentTimezone;
 import br.com.muttley.headers.components.MuttleyCurrentVersion;
 import br.com.muttley.model.security.User;
+import lombok.Getter;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.HashMap;
@@ -14,20 +15,42 @@ import java.util.Map;
  * @project muttley-cloud
  */
 public abstract class AbstractMuttleyReportBuilder<T extends MuttleyReportBuilder> implements MuttleyReportBuilder<T> {
+
+    public static final String CURRENT_USER = "CURRENT_USER";
+    public static final String CURRENT_VERSION = "CURRENT_VERSION";
+    public static final String CURRENT_TIMEZONE = "CURRENT_TIMEZONE";
+
+    @Getter
     protected Map<String, Object> params = new HashMap<>();
+    @Getter
     protected MongoTemplate template;
-    protected User user;
-    protected MuttleyCurrentVersion version;
-    protected MuttleyCurrentTimezone timezone;
-    private final T INSTANCE = (T) this;
+    @Getter
+    protected User currentUser;
+    @Getter
+    protected MuttleyCurrentVersion currentVersion;
+    @Getter
+    protected MuttleyCurrentTimezone currentTimezone;
+
+    protected final T INSTANCE = (T) this;
+
+    protected AbstractMuttleyReportBuilder() {
+    }
+
+    protected AbstractMuttleyReportBuilder(final MuttleyReportBuilder builder) {
+        this.setTemplate(builder.getTemplate());
+        this.setCurrentUser(builder.getCurrentUser());
+        this.setCurrentTimezone(builder.getCurrentTimezone());
+        this.setCurrentVersion(builder.getCurrentVersion());
+    }
 
     public T addParam(final String key, final Object value) {
         this.params.put(key, value);
         return this.INSTANCE;
     }
 
-    public Map<String, Object> getParams() {
-        return this.params;
+    public T removeParam(final String key) {
+        this.params.remove(key);
+        return this.INSTANCE;
     }
 
     public T setTemplate(final MongoTemplate template) {
@@ -35,31 +58,24 @@ public abstract class AbstractMuttleyReportBuilder<T extends MuttleyReportBuilde
         return this.INSTANCE;
     }
 
-    public MongoTemplate getTemplate() {
-        return this.template;
-    }
 
-    public T setUser(final User user) {
-        this.user = user;
+    public T setCurrentUser(final User user) {
+        this.currentUser = user;
         if (user != null) {
-            this.addParam("CURRENT_USER", user.getName());
+            this.addParam(CURRENT_USER, user.getName());
         }
         return this.INSTANCE;
     }
 
-    public User getUser() {
-        return user;
-    }
-
     public T setCurrentVersion(final MuttleyCurrentVersion version) {
-        this.version = version;
-        this.addParam("CURRENT_VERSION", version.getCurrenteFromServer());
+        this.currentVersion = version;
+        this.addParam(CURRENT_VERSION, version.getCurrenteFromServer());
         return this.INSTANCE;
     }
 
     public T setCurrentTimezone(final MuttleyCurrentTimezone timezone) {
-        this.timezone = timezone;
-        this.addParam("CURRENT_TIMEZONE", timezone.getCurrentTimezoneFromRequestOrServer());
+        this.currentTimezone = timezone;
+        this.addParam(CURRENT_TIMEZONE, timezone.getCurrentTimezoneFromRequestOrServer());
         return this.INSTANCE;
     }
 }
--- muttley-report/src/main/java/br/com/muttley/report/MuttleyReportBuilder.java
+++ muttley-report/src/main/java/br/com/muttley/report/MuttleyReportBuilder.java
@@ -1,5 +1,12 @@
 package br.com.muttley.report;
 
+import br.com.muttley.headers.components.MuttleyCurrentTimezone;
+import br.com.muttley.headers.components.MuttleyCurrentVersion;
+import br.com.muttley.model.security.User;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.Map;
+
 /**
  * @author Joel Rodrigues Moreira 10/11/2020
  * <a href="mailto:joel.databox@gmail.com">joel.databox@gmail.com</a>
@@ -7,7 +14,17 @@ package br.com.muttley.report;
  */
 public interface MuttleyReportBuilder<T extends MuttleyReportBuilder> {
 
-    MuttleyReportBuilder addParam(final String key, final Object value);
+    MongoTemplate getTemplate();
+
+    User getCurrentUser();
+
+    MuttleyCurrentVersion getCurrentVersion();
+
+    MuttleyCurrentTimezone getCurrentTimezone();
+
+    T addParam(final String key, final Object value);
+
+    Map<String, Object> getParams();
 
     MuttleyReport build();
 }
