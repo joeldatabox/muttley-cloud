2563ea75e7c78e305c0659e4af1100deb4d06e71
Joel Rodrigues Moreira <joel.databox@gmail.com>
2022-06-22 11:47:19 -0300

    implementação virtualização ao executar relatório

--- muttley-report/src/main/java/br/com/muttley/report/AbstractMuttleyReport.java
+++ muttley-report/src/main/java/br/com/muttley/report/AbstractMuttleyReport.java
@@ -3,11 +3,16 @@ package br.com.muttley.report;
 import br.com.muttley.exception.throwables.MuttleyException;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.fill.JRSwapFileVirtualizer;
+import net.sf.jasperreports.engine.util.JRSwapFile;
+import org.springframework.scheduling.annotation.Async;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
 
+import static java.lang.System.getProperty;
+import static net.sf.jasperreports.engine.JRParameter.REPORT_VIRTUALIZER;
 import static net.sf.jasperreports.engine.JasperExportManager.exportReportToPdfStream;
 import static net.sf.jasperreports.engine.JasperFillManager.fillReport;
 import static net.sf.jasperreports.engine.util.JRLoader.loadObject;
@@ -50,7 +55,17 @@ public abstract class AbstractMuttleyReport implements MuttleyReport {
 
     @Override
     public void print(final OutputStream outputStream) {
+        //criando o virtualizador de cache para impressão
+        final JRSwapFileVirtualizer virtualizer = new JRSwapFileVirtualizer(
+                2,
+                new JRSwapFile(getProperty("java.io.tmpdir"), 1024, 100),
+                true
+        );
         try {
+            //adicionando o virtualizador
+            final Map<String, Object> map = this.getParams();
+            map.put(REPORT_VIRTUALIZER, virtualizer);
+
             exportReportToPdfStream(
                     fillReport(
                             this.loadReport(),
@@ -61,6 +76,8 @@ public abstract class AbstractMuttleyReport implements MuttleyReport {
             );
         } catch (final JRException e) {
             throw new MuttleyException(e);
+        } finally {
+            virtualizer.cleanup();
         }
     }
 
