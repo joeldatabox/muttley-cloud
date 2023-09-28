c946ab0b051f06d7103604941c7f4d612624f357
Joel Rodrigues Moreira <joel.databox@gmail.com>
2023-09-27 21:40:01 -0300

    resolved

--- muttley-notification/src/main/java/br/com/muttley/notification/infobip/service/impl/InfoBitBasicAuthorizationRequestInterceptor.java
+++ muttley-notification/src/main/java/br/com/muttley/notification/infobip/service/impl/InfoBitBasicAuthorizationRequestInterceptor.java
@@ -2,6 +2,8 @@ package br.com.muttley.notification.infobip.service.impl;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
@@ -11,13 +13,24 @@ import org.springframework.beans.factory.annotation.Value;
  */
 public class InfoBitBasicAuthorizationRequestInterceptor implements RequestInterceptor {
     private final String AUTHENTICATION_TOKEN;
+    private final Logger log;
 
-    public InfoBitBasicAuthorizationRequestInterceptor(@Value("${muttley.notification.infobip.token}") final String token) {
+
+    public InfoBitBasicAuthorizationRequestInterceptor(@Value("${muttley.notification.infobip.token:INVALID}") final String token) {
         this.AUTHENTICATION_TOKEN = token;
+        if ("INVALID".equalsIgnoreCase(token)) {
+            this.log = LoggerFactory.getLogger(InfoBitBasicAuthorizationRequestInterceptor.class);
+            this.log.error("\n\n NÃO FOI INFORMADO UM TOKEN VÁLIDO PARA O INFOBIT \n\n");
+        } else {
+            this.log = null;
+        }
     }
 
     @Override
     public void apply(RequestTemplate template) {
         template.header("Authorization", this.AUTHENTICATION_TOKEN);
+        if ("INVALID".equalsIgnoreCase(this.AUTHENTICATION_TOKEN)) {
+            this.log.error("\n\n NÃO FOI INFORMADO UM TOKEN VÁLIDO PARA O INFOBIT \n\n");
+        }
     }
 }
--- muttley-notification/src/main/java/br/com/muttley/notification/twilio/service/impl/TwilioBasicAuthorizationRequestInterceptor.java
+++ muttley-notification/src/main/java/br/com/muttley/notification/twilio/service/impl/TwilioBasicAuthorizationRequestInterceptor.java
@@ -2,6 +2,8 @@ package br.com.muttley.notification.twilio.service.impl;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.codec.Base64;
 
@@ -14,10 +16,21 @@ import java.nio.charset.Charset;
  */
 public class TwilioBasicAuthorizationRequestInterceptor implements RequestInterceptor {
     private static final Charset CHARSET = Charset.forName("ISO-8859-1");
+    private final String user;
+    private final String password;
     private final String AUTHENTICATION;
+    private final Logger log;
 
-    public TwilioBasicAuthorizationRequestInterceptor(@Value("${muttley.notification.twilio.accountSid}") final String user, @Value("${muttley.notification.twilio.accountToken}") final String password) {
+    public TwilioBasicAuthorizationRequestInterceptor(@Value("${muttley.notification.twilio.accountSid:INVALID}") final String user, @Value("${muttley.notification.twilio.accountToken:INVALID}") final String password) {
+        this.user = user;
+        this.password = password;
         this.AUTHENTICATION = base64Encode(user + ":" + password);
+        if ("INVALID".equalsIgnoreCase(user) || "INVALID".equalsIgnoreCase(password)) {
+            this.log = LoggerFactory.getLogger(TwilioBasicAuthorizationRequestInterceptor.class);
+            this.log.error("\n\n NÃO FOI INFORMADO USUÁRIO OU SENHA VÁLIDO PARA O TWILIO \n\n");
+        } else {
+            this.log = null;
+        }
     }
 
     private static String base64Encode(String payload) {
@@ -28,5 +41,8 @@ public class TwilioBasicAuthorizationRequestInterceptor implements RequestInterc
     @Override
     public void apply(RequestTemplate template) {
         template.header("Authorization", this.AUTHENTICATION);
+        if ("INVALID".equalsIgnoreCase(this.user) || "INVALID".equalsIgnoreCase(this.password)) {
+            this.log.error("\n\n NÃO FOI INFORMADO USUÁRIO OU SENHA VÁLIDO PARA O TWILIO \n\n");
+        }
     }
 }
