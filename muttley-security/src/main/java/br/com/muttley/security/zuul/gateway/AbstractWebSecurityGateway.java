e213e4d8f3521d494f8d38569ee2518391765243
cedromaxxsoft <ana.carolina@maxxsoft.com.br>
2024-10-07 15:44:58 -0300

    add static value for endpoint

--- muttley-security/src/main/java/br/com/muttley/security/zuul/gateway/AbstractWebSecurityGateway.java
+++ muttley-security/src/main/java/br/com/muttley/security/zuul/gateway/AbstractWebSecurityGateway.java
@@ -31,7 +31,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public abstract class AbstractWebSecurityGateway extends WebSecurityConfigurerAdapter {
     protected final String loginEndPoint;
     protected final String refreshTokenEndPoin;
-    protected final String forgotPassword;
+    protected final String forgotPasswordEndPoint;
     protected final String createEndPoint;
     protected final String resetPassword;
     protected final UnauthorizedHandler unauthorizedHandler;
@@ -42,7 +42,7 @@ public abstract class AbstractWebSecurityGateway extends WebSecurityConfigurerAd
     public AbstractWebSecurityGateway(
             @Value("${muttley.security.jwt.controller.loginEndPoint}") final String loginEndPoint,
             @Value("${muttley.security.jwt.controller.refreshEndPoint}") final String refreshTokenEndPoin,
-            @Value("${muttley.security.jwt.controller.forgotPassword}") final String forgotPassword,
+            @Value("${muttley.security.jwt.controller.forgotPasswordEndPoint}") final String forgotPassword,
             @Value("${muttley.security.jwt.controller.createEndPoint}") final String createEndPoint,
             @Value("${muttley.security.jwt.controller.resetPassword}") final String resetPassword,
             final UnauthorizedHandler unauthorizedHandler,
@@ -50,7 +50,7 @@ public abstract class AbstractWebSecurityGateway extends WebSecurityConfigurerAd
             final UserServiceClient userServiceClient) {
         this.loginEndPoint = loginEndPoint;
         this.refreshTokenEndPoin = refreshTokenEndPoin;
-        this.forgotPassword = forgotPassword;
+        this.forgotPasswordEndPoint = forgotPassword;
         this.createEndPoint = createEndPoint;
         this.resetPassword = resetPassword;
         this.unauthorizedHandler = unauthorizedHandler;
@@ -94,7 +94,7 @@ public abstract class AbstractWebSecurityGateway extends WebSecurityConfigurerAd
                         this.endPointPermitAllToGet()
                 ).permitAll()
                 //permitindo acesso aos endpoint de login
-                .antMatchers(loginEndPoint, refreshTokenEndPoin, forgotPassword, createEndPoint, resetPassword).permitAll()
+                .antMatchers(loginEndPoint, refreshTokenEndPoin, forgotPasswordEndPoint, createEndPoint, resetPassword).permitAll()
                 //barrando qualquer outra requisição não autenticada
                 .anyRequest().authenticated();
 
