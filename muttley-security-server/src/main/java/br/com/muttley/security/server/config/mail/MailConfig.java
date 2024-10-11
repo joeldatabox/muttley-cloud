e7b66ebdd75a2705859bcb57d036782b6b89710b
cedromaxxsoft <ana.carolina@maxxsoft.com.br>
2024-10-11 20:10:01 -0300

    ajustes .env

--- muttley-security-server/src/main/java/br/com/muttley/security/server/config/mail/MailConfig.java
+++ muttley-security-server/src/main/java/br/com/muttley/security/server/config/mail/MailConfig.java
@@ -1,4 +1,7 @@
 package br.com.muttley.security.server.config.mail;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +13,19 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    private final Dotenv dotenv = Dotenv.configure().load();
+
+
+
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
+        mailSender.setHost("smtp.maxxsoft.com.br");
         mailSender.setPort(587);
 
-        mailSender.setUsername("manobixagency@gmail.com");
-        mailSender.setPassword("hwnt hvio sngi qtjn");
+        mailSender.setUsername("agrifocus@maxxsoft.com.br");
+        mailSender.setPassword(dotenv.get("EMAIL_PASSWORD_MAXXSOFT"));
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
